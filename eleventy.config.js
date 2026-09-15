83e572a25f9ad7c72b3dfca289075c3809f2c5bc
ecivallero <155031739+ecivallero@users.noreply.github.com>
2026-09-15 12:31:59 -0500

    Add files via upload

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,8 @@ export default function (eleventyConfig) {
   // Files/directories that Eleventy should copy unchanged into _site/.
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("favicon.png");
 
   // Repository documentation is for us, not part of the public site.
   eleventyConfig.ignores.add("README.md");
